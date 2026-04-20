fa4d1e3d3282dd012a5325407287ce47a8c3e822
泰輝 松崎 <oibc37035@ojs.ac.jp>
2026-04-20 10:22:57 +0900

    繰り返し回数の表示

--- Sample0416_1/Program.cs
+++ Sample0416_1/Program.cs
@@ -5,11 +5,13 @@
         static void Main(string[] args)
         {
             int i = 0;
-            while (i < 10) {
+            while (i < 10)
+            {
 
                 i++;
                 Console.WriteLine("Hello, World!");
             }
+            Console.WriteLine(i + "回繰り返した");
         }
     }
 }
