15948fc97efb30a4b41341705c6ec82ade408050
泰輝 松崎 <oibc37035@ojs.ac.jp>
2026-04-23 09:51:42 +0900

    staticを入れて静的classにした

--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -22,32 +22,27 @@ namespace DistanceConverter {
             }
 
         }
-        FeetConverter converter = new FeetConverter();
-       
-         static void printMeterToFeetList(int start, int stop) {
-            FeetConverter converter = new FeetConverter();
+
+
+        static void printMeterToFeetList(int start, int stop) {
+
             //メートルからフィートへの対応表を出力
             for (int meter = start; meter <= stop; meter++) {
-                double feet = converter.FromMeter(meter);
+                double feet = FeetConverter.FromMeter(meter);
                 Console.WriteLine($"{meter}m = {feet:0.0000}ft");
             }
 
         }
         static void printFeetToMeterList(int start, int stop) {
-            FeetConverter converter = new FeetConverter();
+
 
             //フィートからメートルへの対応表を出力
             for (int feet = start; feet <= stop; feet++) {
-                double meter =converter.ToMeter(feet);
+                double meter = FeetConverter.ToMeter(feet);
                 Console.WriteLine($"{feet}ft = {meter:0.0000}m");
             }
 
         }
-
-       
-
-
-
 
     }
 }
