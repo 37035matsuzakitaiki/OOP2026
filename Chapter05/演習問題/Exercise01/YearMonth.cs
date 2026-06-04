b0a379a60cdf65bb082ea862cf236a56cade53f4
泰輝 松崎 <oibc37035@ojs.ac.jp>
2026-06-04 10:20:16 +0900

    5.13,4完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -19,15 +19,19 @@ namespace Exercise01 {
         //5.1.2(P116参照)
         //設定されている西暦が21世紀か判定する
         //Yearが2001～2100年の間ならtrue、それ以外ならfalseを返す
-        public bool Is21Century =>2001 <= Year && Year <= 2100;
+        public bool Is21Century => 2001 <= Year && Year <= 2100;
 
         //5.1.3
         public YearMonth AddOneMonth() {
-
+            if (Month <= 11) {
+                return new YearMonth(Year, Month + 1);
+            } else {
+                return new YearMonth(Year + 1, 1);
+            }
         }
 
         //5.1.4
-        public override string ToString() => ;
+        public override string ToString() => $"{Year}年{Month}月";
 
     }
 }
