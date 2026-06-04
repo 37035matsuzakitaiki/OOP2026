800721f6f49dc2760a57947500e9bb50d68e058e
泰輝 松崎 <oibc37035@ojs.ac.jp>
2026-06-04 09:42:07 +0900

    イディオム完成

---
+++ Chapter05/プロパティに関するイディオム/Section01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Immutable;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            //var obj = new PasswordPolicy("aaaaa", "bbbbbb");
+            //var data = obj.Name;
+
+            var ms = new MySample();
+            //変更不可のオブジェクトなので、Add,RemoveATは新たなインスタンスを返す
+            var newList = ms.MyList.Add(6).RemoveAt(0);
+            ms.MyList.ForEach(n => Console.WriteLine($"{n}"));
+            Console.WriteLine();//改行
+            newList.ForEach(n => Console.WriteLine($"{n}"));
+            Console.WriteLine();
+
+            
+
+           
+        }
+    }
+       class MySample {
+        public ImmutableList<int> MyList { get;set; }
+
+        public MySample() {
+            var list = new List<int>() { 1, 2, 3, 4, 5 };
+            MyList = list.ToImmutableList();
+        }
+    }
+   
+
+
+    class PasswordPolicy {
+        //プロパティの初期化
+        public int MinimumLength { get; set; } = 8;
+
+        //読み取り専用プロパティ
+        public string GivenName { get; init; } = null!;
+         
+        public string FamilyName { get; init; } = null!;
+
+        public PasswordPolicy(string familyName, string givenName) {
+            FamilyName = familyName;
+            GivenName = givenName;
+        }
+    }
+
+}
---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //5.1.1
+    internal class YearMonth {
+        //プロパティ(P114参照)
+        public int Year { get; init; }
+        public int Month { get; init; }
+
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+
+        //5.1.2(P116参照)
+        //設定されている西暦が21世紀か判定する
+        //Yearが2001～2100年の間ならtrue、それ以外ならfalseを返す
+        public bool Is21Century =>2001 <= Year && Year <= 2100;
+
+        //5.1.3
+        public YearMonth AddOneMonth() {
+
+        }
+
+        //5.1.4
+        public override string ToString() => ;
+
+    }
+}
