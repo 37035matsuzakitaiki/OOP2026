760490cddf1c52f500173c849c75a8719e1511ba
泰輝 松崎 <oibc37035@ojs.ac.jp>
2026-06-08 11:18:03 +0900

    5章まで終了

---
+++ Chapter05/演習問題/新しいフォルダー/Exercise03/Program.cs
@@ -0,0 +1,17 @@
+﻿using Exercise01;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var ym1 = new YearMonth(2002, 10);
+            var ym2 = new YearMonth(2002, 10);
+            if (ym1==ym2) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+
+        }        
+    }
+}
