128d4e75dc4ae0c7ec4b69483da0d2ae842eb774
泰輝 松崎 <oibc37035@ojs.ac.jp>
2026-06-11 09:59:56 +0900

    6_5まで完成

---
+++ Chapter06/文字列の比較/Section01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var str1 = "コマンド";
+            var str2 = "ｺﾏﾝﾄﾞ";
+
+            var cultureInfo = new CultureInfo("ja-JP");
+
+            if (String.Compare(str1, str2,cultureInfo,
+                CompareOptions.IgnoreWidth | CompareOptions.IgnoreCase)==0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+
+        }
+    }
+}
