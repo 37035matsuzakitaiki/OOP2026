61ededfa8a4215d2795c98c57943d213d6fe9b16
泰輝 松崎 <oibc37035@ojs.ac.jp>
2026-06-11 11:33:20 +0900

    配列を用いた集計

---
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -0,0 +1,95 @@
+﻿
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+            #region
+            Console.WriteLine("6.3.1");
+            Exercise1(text);
+
+            Console.WriteLine("6.3.2");
+            Exercise2(text);
+
+            Console.WriteLine("6.3.3");
+            Exercise3(text);
+
+            Console.WriteLine("6.3.4");
+            Exercise4(text);
+
+            Console.WriteLine("6.3.5");
+            Exercise5(text);
+
+            Console.WriteLine("6.3.99");
+            Exercise6(text);
+            #endregion
+        }
+
+        private static void Exercise1(string text) {
+            var spaces = text.Count(c => c == ' ');
+            Console.WriteLine($"空白数:{spaces}");
+            //別の書き方
+            //Console.WriteLine("空白数:{0}", spaces);
+        }
+
+        private static void Exercise2(string text) {
+            Console.Write("検索：");
+            var search = Console.ReadLine();
+            Console.Write("置換：");
+            var replace = Console.ReadLine();
+
+            var replaced = text.Replace(search, replace);
+            Console.WriteLine(replaced);
+        }
+
+        private static void Exercise3(string text) {
+            //やらなくてよい
+        }
+
+        private static void Exercise4(string text) {
+           // var spaces = text.Count(c => c == ' ');
+           // Console.WriteLine($"単語数:{spaces + 1}");
+            var count = text.Split(' ').Length;
+            Console.WriteLine($"単語数：{count}");
+        }
+
+        private static void Exercise5(string text) {
+            //テスト出るらしい
+             text.Split(' ').Where(s => s.Length <= 4).ToList().ForEach(Console.WriteLine);
+             
+        }
+
+        //アルファベットの数をカウントして表示する
+        private static void Exercise6(string text) {
+            var str = text.ToLower().Replace(" ", "");
+            //ディクショナリーを使った集計
+            var alphDicCount = Enumerable.Range('a', 26).
+            ToDictionary(num => ((char)num),num => 0);
+
+           // var dict = new SortedDictionary<char,int>();
+
+            foreach (var c in str) {  
+                    alphDicCount[c]++;
+            }
+
+
+            foreach (var word in alphDicCount) {
+                Console.WriteLine(word.Key + ":" + word.Value);
+            }
+
+            Console.WriteLine();  //改行
+            //配列を用いた集計
+            var array = Enumerable.Repeat(0, 26).ToArray();
+            foreach (var alph in str) {
+                array[alph - 'a']++;
+            }
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                Console.WriteLine($"{ch}:{array[ch - 'a']}");
+            }
+
+            foreach (var num in array) {
+                Console.WriteLine(num);
+            }
+
+        }
+    }
+}
