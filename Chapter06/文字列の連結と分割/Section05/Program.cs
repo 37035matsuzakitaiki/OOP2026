d8a5a3b16b9ea1e4a849ba20b984482d319a0447
泰輝 松崎 <oibc37035@ojs.ac.jp>
2026-06-15 10:36:46 +0900

    配列区切りjoin

---
+++ Chapter06/文字列の連結と分割/Section05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var result = String.Join(",",GetWords());
+            Console.WriteLine(result);
+            
+            //var text = GetWords.Split(new[] {','},
+            //    StringSplitOptions.RemoveEmptyEntries) ;
+            //Console.WriteLine(sb);
+        }
+
+        private static IEnumerable<object> GetWords() {
+            return ["Orange", "Lemon", "Strawberry"];
+        }
+    }
+}
