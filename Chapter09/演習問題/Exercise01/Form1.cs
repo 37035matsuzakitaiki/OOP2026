b1f075f8b61c33d536a179fe0f8a8131c190a962
泰輝 松崎 <oibc37035@ojs.ac.jp>
2026-07-09 14:43:36 +0900

    9.1 完成

--- Chapter09/演習問題/Exercise01/Form1.cs
+++ Chapter09/演習問題/Exercise01/Form1.cs
@@ -1,7 +1,26 @@
+using System.Globalization;
+
 namespace Exercise01 {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+        }
+
+        private void btButton1_Click(object sender, EventArgs e) {
+            DateTime today = DateTime.Now;
+            tbOut1.Text = $"{today}";
+        }
+
+        private void btButton2_Click(object sender, EventArgs e) {
+            DateTime today = DateTime.Now;
+            tbOut2.Text = today.ToString("yyyy”NMŒŽd“ú HHŽžmm•ªss•b");
+        }
+
+        private void btButton3_Click(object sender, EventArgs e) {
+            DateTime today = DateTime.Now;
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            tbOut3.Text = today.ToString($"ggy”N MŒŽ d“ú (dddd)",culture);
         }
     }
 }
