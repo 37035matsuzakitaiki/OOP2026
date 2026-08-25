ce2e88bbd312fdae333b516314be723a702c6b59
泰輝 松崎 <oibc37035@ojs.ac.jp>
2026-08-25 14:30:52 +0900

    MainFormBackColorの初期値設定

--- FromApps/CarReportSystem/Setting.cs
+++ FromApps/CarReportSystem/Setting.cs
@@ -1,6 +1,8 @@
 ﻿
 namespace CarReportSystem {
     public class Settings {
+        //メイン画面に設定した色情報
         public int MainFormBackColor { get; set; }
+        = SystemColors.Control.ToArgb();
     }
 }
